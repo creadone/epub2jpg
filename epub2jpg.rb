52a3f1ce77181947df0391d0155deafdfb37e8a0
creadone <creadone@gmail.com>
2014-12-23 16:38:11 +0300

    limit treads num

--- epub2jpg.rb
+++ epub2jpg.rb
@@ -1,11 +1,11 @@
 #run script from dir with *.epub (name: 763254_epub.epub)
 
-start = Time.now
+require "in_threads"
 
-CALIBRE = 'ebook-convert ' #calibre-ebook.com
+CALIBRE = '/Applications/calibre.app/Contents/MacOS/ebook-convert ' #calibre-ebook.com
 CONVERT = 'convert' #imagemagick
 PAGETOPREVIEW = 10 #in %
-threads = []
+THREADSNUM = 20
 sourceBooks = Dir.glob('*.epub').sort!
 
 def getBookID (filename)
@@ -30,31 +30,21 @@ end
 def getBookPreview (bookname)
   unless File.directory?(getBookID(bookname))
     system('mkdir -p '+getBookID(bookname)+'/jpg/')
-    print "\nDirs #{getBookID(bookname)}/jpg/ for images done\n"
+    print "\nDirs #{getBookID(bookname)}/jpg/ for images done\n\n"
     system('chmod 777 '+getBookID(bookname)+'/jpg/')
-    print "Rights id done\n"
-    print "Converting #{getBookID(bookname)}: epub -> pdf...\n"
+    print "Rights is assigned\n\n"
+    print "Converting #{getBookID(bookname)}: epub -> pdf...\n\n"
     system(CALIBRE+bookname+' '+getBookID(bookname)+'/'+getBookID(bookname)+'.pdf'+' --extra-css styles/style.css >>calibre_log.txt 2>&1')
-    print "epub to #{getBookID(bookname)}.pdf converted\n"
-    print "#{getBookID(bookname)}: create images for preview...\n"
+    print "epub to #{getBookID(bookname)}.pdf converted\n\n"
+    print "#{getBookID(bookname)}: create images for preview...\n\n"
     system(CONVERT+' -scale 2000x1000 -quality 100 -density 72 -bordercolor white -border 10 '+getBookID(bookname)+'/'+getBookID(bookname)+'.pdf '+getBookID(bookname)+'/jpg/'+getBookID(bookname)+"_%03d.jpg"+" >>convert_log.txt 2>&1")
-    print "#{getBookID(bookname)}: images for all pages is done\n"
+    print "#{getBookID(bookname)}: images for all pages is done\n\n"
     removeExtraFiles(getBookID(bookname))
     print "Preview for book #{getBookID(bookname)} is done\n\n"
   end
 end
 
-sourceBooks.each do |file|
-  threads << Thread.new {
+sourceBooks.in_threads(THREADSNUM).map do |file|
     getBookPreview(file)
-  }
 end
 
-threads.each { |thr| thr.join }
-
-finish = Time.now
-diff = finish - start
-
-print "---------------------------------"
-print "All books is done: #{diff} seconds"
-print "---------------------------------"
