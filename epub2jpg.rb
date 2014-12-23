bb53d627490a0932093c4ac37d44b1032dfea731
creadone <creadone@gmail.com>
2014-12-23 12:22:45 +0300

    Add threads

--- epub2jpg.rb
+++ epub2jpg.rb
@@ -5,7 +5,7 @@ start = Time.now
 CALIBRE = 'ebook-convert ' #calibre-ebook.com
 CONVERT = 'convert' #imagemagick
 PAGETOPREVIEW = 10 #in %
-
+threads = []
 sourceBooks = Dir.glob('*.epub').sort!
 
 def getBookID (filename)
@@ -33,20 +33,24 @@ def getBookPreview (bookname)
     print "\nDirs #{getBookID(bookname)}/jpg/ for images done\n"
     system('chmod 777 '+getBookID(bookname)+'/jpg/')
     print "Rights id done\n"
-    print "Converting epub -> pdf...\n"
+    print "Converting #{getBookID(bookname)}: epub -> pdf...\n"
     system(CALIBRE+bookname+' '+getBookID(bookname)+'/'+getBookID(bookname)+'.pdf'+' --extra-css styles/style.css >>calibre_log.txt 2>&1')
     print "epub to #{getBookID(bookname)}.pdf converted\n"
-    print "Making images for preview...\n"
+    print "#{getBookID(bookname)}: create images for preview...\n"
     system(CONVERT+' -scale 2000x1000 -quality 100 -density 72 -bordercolor white -border 10 '+getBookID(bookname)+'/'+getBookID(bookname)+'.pdf '+getBookID(bookname)+'/jpg/'+getBookID(bookname)+"_%03d.jpg"+" >>convert_log.txt 2>&1")
-    print "Images for all pages is done\n"
+    print "#{getBookID(bookname)}: images for all pages is done\n"
     removeExtraFiles(getBookID(bookname))
     print "Preview for book #{getBookID(bookname)} is done\n\n"
   end
 end
 
 sourceBooks.each do |file|
-  getBookPreview(file)
+  threads << Thread.new {
+    getBookPreview(file)
+  }
 end
+
+threads.each { |thr| thr.join }
 
 finish = Time.now
 diff = finish - start
